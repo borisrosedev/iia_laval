38cc9076cbd870e3a9cfbbcba99b6be72328259b
johanncome <johann.come@iia-formation.fr>
2026-04-15 13:34:30 +0200

    Commit1

---
+++ node-api/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const app = express()
+
+app.set('port', process.env.PORT);
+app.set('host', process.env.HOST);
+
+app.use(express.json({ limit: '10mb'}))
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'All Good!' })
+})
+
+app.listen(app.get('port'), () => {
+    console.log(`Server running at http://${app.get('host')}:${app.get('port')}/`)
+})
---
+++ node_init.sh
@@ -0,0 +1,10 @@
+create_node_api_boilerplate(){
+    cd node-api
+    # Initalise un package.json
+    npm init -y 
+    npm install -D cors express dotenv
+    touch server.js
+    npm pkg set scripts.dev="node --watch server.js"
+}
+
+create_node_api_boilerplate
