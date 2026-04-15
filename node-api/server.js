2cd73abc9a2f08d4f07eaeb8161042e703f9ac88
borisrosedev <boris.rose.dev@gmail.com>
2026-04-15 14:31:50 +0200

    feat/api-2

--- node-api/server.js
+++ node-api/server.js
@@ -6,6 +6,27 @@ const app = express()
 
 app.set('port', process.env.PORT);
 app.set('host', process.env.HOST);
+
+
+//app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '10mb'}))
+
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "All Good" })
+})
+
+app.post("/api/v1/users", (req, res) => {
+    const body = req.body
+    const email = body.email 
+    const password = body.password
+    console.log("Server log: email ==> " + email)
+    console.log("Server log: password ==> " + password)
+    return res.status(201).json({ message: "user created"})
+})
+
+
+
 app.listen(app.get('port'),() => {
     console.log(`Server runs at http://${app.get('host')}:${app.get('port')}`)
 })
