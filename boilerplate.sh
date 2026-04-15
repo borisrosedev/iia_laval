0180f45b7115983c08095316f618e40751cee826
wawan-dev <70236956+wawan-dev@users.noreply.github.com>
2026-04-15 09:28:13 +0200

    push commit

---
+++ boilerplate.sh
@@ -0,0 +1,23 @@
+# /bin/bash
+
+#!/bin/bash
+set -euo pipefail
+
+create_boilerplate() {
+    mkdir -p topology-"$1"/switches/core/interfaces
+    mkdir -p topology-"$1"/switches/access/marketing/interfaces
+    mkdir -p topology-"$1"/switches/access/rh/interfaces
+    mkdir -p topology-"$1"/switches/access/direction/interfaces
+    mkdir -p topology-"$1"/routers/router-1
+    touch topology-"$1"/switches/vlans.config
+    touch topology-"$1"/switches/access/marketing/interfaces/hosts.config
+    touch topology-"$1"/switches/access/marketing/interfaces/core.config
+    touch topology-"$1"/switches/access/rh/interfaces/hosts.config
+    touch topology-"$1"/switches/access/rh/interfaces/core.config
+    touch topology-"$1"/switches/access/direction/interfaces/hosts.config
+    touch topology-"$1"/switches/access/direction/interfaces/core.config
+    touch topology-"$1"/switches/core/interfaces/access.config
+    touch topology-"$1"/switches/core/interfaces/core.config
+}
+
+create_boilerplate "$1"
