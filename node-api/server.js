2762073ac9a45955c3c308bffe3ec258779b0d7f
baptiste <baptistelorillu@gmail.com>
2026-04-15 13:35:37 +0200

    maj

--- node-api/server.js
+++ node-api/server.js
@@ -6,6 +6,13 @@ const app = express()
 
 app.set('port', process.env.PORT);
 app.set('host', process.env.HOST);
+
+app.use(express.json({ limit: '10mb'}))
+app.get("/", (req, res) => {
+    res.status(200).json ({message: "All Good"})
+})
+
+
 app.listen(app.get('port'),() => {
     console.log(`Server runs at http://${app.get('host')}:${app.get('port')}`)
 })
