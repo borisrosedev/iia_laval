f3592fe42033c580f2d3b3f896cc2e565d09c978
aizon <aizon0@proton.me>
2026-04-15 11:56:19 +0200

    express server

---
+++ node-api/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('host', process.env.HOST || 'localhost');
+
+app.listen(app.get('port'), app.get('host'), 0, () => {
+    console.info(`Server is running at ${app.get('host')}:${app.get('port')}! Say hi!`);
+});
