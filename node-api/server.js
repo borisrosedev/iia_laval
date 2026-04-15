a5fbbd751e45c15ff03428c546ffc48ba500e553
aizon <aizon0@proton.me>
2026-04-15 12:26:32 +0200

    node ws http response + capture

--- node-api/server.js
+++ node-api/server.js
@@ -7,6 +7,14 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('host', process.env.HOST || 'localhost');
 
+app.use(express.json({
+    limit: '1mb'
+}));
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'hell yeah brother' });
+});
+
 app.listen(app.get('port'), app.get('host'), 0, () => {
     console.info(`Server is running at ${app.get('host')}:${app.get('port')}! Say hi!`);
 });
