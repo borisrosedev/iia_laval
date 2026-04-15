5d4229618f5e9286a6001bb3f8a42e01e968e693
wawan-dev <70236956+wawan-dev@users.noreply.github.com>
2026-04-15 14:04:23 +0200

    Push avec le culr POST

--- node-api/server.js
+++ node-api/server.js
@@ -12,7 +12,16 @@ app.get("/", (req, res) => {
     res.status(200).json({message:"C'est Okay"})
 })
 
+app.post("/api/v1/users", (req, res) => {
+    const body = req.body
+    const email = body.email
+    const password = body.password
+    console.log("Serveur log: email = "+ email +" password = "+ password)
+    return res.status(201).json({message:"user crée"})
+})
+
 app.listen(app.get('port'), () =>{
     console.log(`Le serveur tourne sur http://${app.get('host')}:${app.get('port')}`);
 })
 
+// Invoke-RestMethod -Uri "http://localhost:3000/api/v1/users" -Method POST -ContentType "application/json" -Body '{ "email":"test@example.com", "password":"monSuperMotDePasse" }'
