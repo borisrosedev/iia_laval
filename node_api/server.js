e5d7c6274757850c9b44509374684099691aa77d
mathias <mathiasgonsard@gmail.com>
2026-04-15 14:08:01 +0200

    Modif_Serverjs+FiltreWire

--- node_api/server.js
+++ node_api/server.js
@@ -12,6 +12,15 @@ app.get("/", (req, res)=>{
     res.status(200).json({message: "All Good"})
 })
 
+app.post("/api/v1/users",(req, res)=> {
+    const body = req.body
+    const email = req.email
+    const password = req.password
+    console.log("Server log: email ==>" + email)
+    console.log("Server log: password ==>" + password)
+    return res.status(201).json({message: "user created"})
+})
+
 app.listen(app.get('port'),() => {
     console.log(`Server runs at http://${app.get('host')}:${app.get('port')}`)
 })
